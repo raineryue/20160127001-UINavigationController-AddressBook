dac1d12a2bd745f9562e77175c893d1251b5405a
raineryue <jinlei_yue@sina.com>
2016-01-27 15:16:26 +0800

    Initial Commit

---
+++ 20160127001-UINavigationController-AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160127001-UINavigationController-AddressBook
+//
+//  Created by Rainer on 16/1/27.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160127001-UINavigationController-AddressBookTests/_0160127001_UINavigationController_AddressBookTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160127001_UINavigationController_AddressBookTests.m
+//  20160127001-UINavigationController-AddressBookTests
+//
+//  Created by Rainer on 16/1/27.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160127001_UINavigationController_AddressBookTests : XCTestCase
+
+@end
+
+@implementation _0160127001_UINavigationController_AddressBookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
