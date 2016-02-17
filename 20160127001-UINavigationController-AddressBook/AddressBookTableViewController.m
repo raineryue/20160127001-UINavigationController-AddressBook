3eadffdf1795565ff2ab710164005ec3ede61310
raineryue <jinlei_yue@sina.com>
2016-02-17 11:15:42 +0800

    完善私人通讯录功能
    完善私人通讯录功能

--- 20160127001-UINavigationController-AddressBook/AddressBookTableViewController.m
+++ 20160127001-UINavigationController-AddressBook/AddressBookTableViewController.m
@@ -32,6 +32,14 @@
     
     // 设置当前表的表格行的分割线（设置为没有分割线）
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    // 添加按钮获取
+    UIBarButtonItem *addBarButtonItem = self.navigationItem.rightBarButtonItem;
+    // 编辑按钮创建
+    UIBarButtonItem *editBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(editBarButtonItemClickAction:)];
+    
+    // 添加导航栏右边按钮
+    self.navigationItem.rightBarButtonItems = @[addBarButtonItem, editBarButtonItem];
 }
 
 #pragma mark - 表格数据源代理方法
@@ -56,6 +64,46 @@
     return cell;
 }
 
+/**
+ *  返回编辑表格行的编辑样式
+ */
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == self.addressBookArray.count - 1)
+        return UITableViewCellEditingStyleInsert;
+    else
+        return UITableViewCellEditingStyleDelete;
+}
+
+/**
+ *  提交编辑表格行
+ */
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (UITableViewCellEditingStyleDelete == editingStyle) { // 删除操作
+        [self.addressBookArray removeObjectAtIndex:indexPath.row];
+        
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationRight];
+    } else { // 新增操作
+        // 模拟一个联系人信息
+        NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+        
+        dictionary[@"userName"] = @"测试人员";
+        dictionary[@"phoneNum"] = @"18818888888";
+        
+        AddressBookModel *addressBook = [AddressBookModel addressBookWithDictionary:dictionary];
+        
+        // 将联系人信息添加到联系人列表中
+        [self.addressBookArray addObject:addressBook];
+        
+        // 创建一个indexPath
+        NSIndexPath *insertIndexPath = [NSIndexPath indexPathForRow:indexPath.row + 1 inSection:0];
+        // 添加一个新的表格行到表格上
+        [tableView insertRowsAtIndexPaths:@[insertIndexPath] withRowAnimation:UITableViewRowAnimationRight];
+    }
+    
+    // 数据归档
+    [NSKeyedArchiver archiveRootObject:self.addressBookArray toFile:kDataPath];
+}
+
 #pragma mark - 代理方法
 /**
  *  添加联系人代理方法
@@ -75,7 +123,11 @@
  *  修改联系人代理方法
  */
 - (void)addressBookEditViewController:(AddressBookEditViewController *)viewController addressBook:(AddressBookModel *)addressBook {
+    // 刷新表格
     [self.tableView reloadData];
+    
+    // 数据归档
+    [NSKeyedArchiver archiveRootObject:self.addressBookArray toFile:kDataPath];
 }
 
 #pragma mark - 控制器跳转事件
@@ -121,6 +173,13 @@
     // 4.显示提示框按钮
     [self presentViewController:alertController animated:YES completion:nil];
 #endif
+}
+
+/**
+ *  编辑按钮点击事件
+ */
+- (void)editBarButtonItemClickAction:(UIBarButtonItem *)barButtonItem {
+    [self.tableView setEditing:!self.tableView.editing animated:YES];
 }
 
 #pragma mark - 属性懒加载
